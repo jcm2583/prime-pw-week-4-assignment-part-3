9b4c85d750678cbe128716385418c84759e9b78f
Jacob Motes <jmotes2583@gmail.com>
2021-04-23 18:11:37 -0500

    completed assignment with stretch goal attempts

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,26 +1,9 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// I started off with stretch goals 1 and 2 here
 let basket = [];
-
-function addItem (item) {
-  basket.push(item);
-  return true;
-}
-
-function listItems (array) {
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i]); }
-}
-
-function empty (array) {
-  array.length = 0;
-}
-
-console.log(`Basket is ${basket}`);
-console.log('Adding apples (expect true)', addItem('apples'));
-console.log(`Basket is now ${basket}`);
-
 let maxItems = 5;
 
 function isFull (array) {
@@ -31,3 +14,46 @@ function isFull (array) {
   }
 }
 console.log(isFull(basket));
+
+function addItem (item) {
+  if (isFull(basket) == false) {
+  basket.push(item);
+    return true; }
+  else {
+    return false;
+  }
+}
+
+function listItems (array) {
+  for (let i = 0; i < array.length; i++) {
+   console.log(array[i]); }
+}
+
+function empty (array) {
+  array.length = 0;
+}
+
+addItem('pears');
+addItem('oranges');
+listItems(basket);
+
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`The updated basket has ${basket}`);
+console.log('The basket is now', empty(basket));
+
+// Adding items back into basket
+addItem('pears');
+addItem('oranges');
+addItem('apples');
+listItems(basket);
+
+function removeItem (item) {
+  let removed = basket.indexOf(item);
+  if (removed == basket.indexOf(item)) {
+  basket.splice(removed);
+  }
+}
+
+removeItem('pears');
+console.log(basket);
